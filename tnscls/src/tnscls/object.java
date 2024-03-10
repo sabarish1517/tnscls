89d523cb8a92e3a3f0054d945803de9a0620c010
ADMIN <ADMIN@DESKTOP-C84O6OC>
2024-03-10 10:38:43 +0530

    inital commit

---
+++ tnscls/src/tnscls/object.java
@@ -0,0 +1,13 @@
+package tnscls;
+
+public class object {
+
+	public static void main(String[] args) {
+		Customer c1=new Customer();
+		c1.setName("sabarish");
+		c1.setId(23);
+		System.out.println("name:"+c1.getName());
+
+	}
+
+}
